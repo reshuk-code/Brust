e956406cbfcb5d8cf9d2c97b0b7eddc290f0f884
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2024-09-02 12:54:38 +0545

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -26,7 +26,7 @@ const MainContent = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <a className="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/shop">Energy Drinks</a>
